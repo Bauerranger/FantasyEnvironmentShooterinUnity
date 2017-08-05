c112dc69a0233b0e87c977b709734c9509b9398a
Bauerranger <Torben.Borghoff@stud.h-da.de>
2017-08-05 14:21:33 +0200

    Music

--- Assets/Scripts/NetworkEnemySpawn.cs
+++ Assets/Scripts/NetworkEnemySpawn.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NetworkEnemySpawn : MonoBehaviour {
-
+    //somehow, if this is used, the spawned enemys do not work properly (get no damage/ don´t change states etc.)
     List<EnemySpawn> enemySpawns = new List<EnemySpawn>();
 	void Start () {
         enemySpawns.AddRange(GetComponentsInChildren<EnemySpawn>());
