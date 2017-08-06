2aedd70af4bcba0a9cc1273042329586e7a1b905
Bauerranger <Torben.Borghoff@stud.h-da.de>
2017-08-06 22:31:48 +0200

    Finalised Shots. Up next: Leveldesign

--- Assets/Scripts/Shots/FastShot.cs
+++ Assets/Scripts/Shots/FastShot.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class FastShot : NormalShot
 {
 
     [SerializeField]
-    private ShotBase[] shots;
+    private GameObject[] shots;
     [SerializeField]
     private float waitTime = 0.01f;
 
@@ -15,9 +16,39 @@ public class FastShot : NormalShot
         StartCoroutine(WaitBetweenShots(waitTime));
     }
 
+    public void Collide(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            foreach (GameObject hit in hitEnemyAnimations)
+            {
+                GameObject spawnedParticle = Instantiate(hit, collision.transform.position, Quaternion.identity) as GameObject;
+            }
+            collision.gameObject.GetComponent<Animator>().SetTrigger("Take Damage");
+        }
+        else
+        {
+            foreach (GameObject hit in hitEnvironmentAnimations)
+            {
+                GameObject spawnedParticle = Instantiate(hit, collision.transform.position, Quaternion.identity) as GameObject;
+            }
+        }
+
+        Destroy(gameObject);
+        if (!NetworkServer.active)
+        {
+            return;
+        }
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyController>().TakeDamage(shotDamage, player);
+        }
+    }
+
     IEnumerator WaitBetweenShots(float waitTime)
     {
-        foreach (ShotBase shot in shots)
+        foreach (GameObject shot in shots)
         {
             shotRigidbody = shot.GetComponent<Rigidbody>();
             shotRigidbody.AddForce(transform.forward * FireForce);
---
+++ Assets/Scripts/Shots/FastShotProjectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastShotProjectile : MonoBehaviour {
+
+    [SerializeField]
+    GameObject SpawnedFrom;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        SpawnedFrom.GetComponent<FastShot>().Collide(collision);
+    }
+}
