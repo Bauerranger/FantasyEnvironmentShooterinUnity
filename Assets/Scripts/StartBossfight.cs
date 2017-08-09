903349799c7c2ba39b247f7d3b164a94f343fd0f
Bauerranger <Torben.Borghoff@stud.h-da.de>
2017-08-09 18:21:44 +0200

    Changed dependencies in the boss battle start behaviour

--- Assets/Scripts/StartBossfight.cs
+++ Assets/Scripts/StartBossfight.cs
@@ -7,6 +7,7 @@ public class StartBossfight : MonoBehaviour {
     public GameObject gameManager;
     public GameObject boss;
     private int count;
+    private GameObject player;
 
     /// <summary>
     /// Starts boss fight
@@ -14,12 +15,13 @@ public class StartBossfight : MonoBehaviour {
     /// <param name="other"></param>
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag != "Player")
+        if (other.tag != "Player" || other.gameObject == player)
             return;
         gameManager.GetComponent<GameManagerScript>().inBossfight = true;
         boss.GetComponent<EnemyController>().isInBossfight = true;
         count++;
         if (count >= GameObject.FindGameObjectsWithTag("Player").Length)
             GetComponent<BoxCollider>().enabled = true;
+        player = other.gameObject;
     }
 }
